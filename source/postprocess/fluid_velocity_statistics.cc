d558815d452a283d7953dc54e010308dfe7c0ef4
danieldouglas92 <daniel.douglas@student.nmt.edu>
2025-06-12 10:04:38 -0600

    add changelog

--- source/postprocess/fluid_velocity_statistics.cc
+++ source/postprocess/fluid_velocity_statistics.cc
@@ -35,57 +35,89 @@ namespace aspect
     std::pair<std::string,std::string>
     FluidVelocityStatistics<dim>::execute (TableHandler &statistics)
     {
+      AssertThrow(this->introspection().compositional_name_exists("porosity"),
+                  ExcMessage("The 'fluid velocity statistics' postprocessor requires a "
+                             "compositional field called porosity."));
+
+      const unsigned int porosity_idx = this->introspection().find_composition_type(CompositionalFieldDescription::porosity);
+
+      AssertThrow(this->get_parameters().include_melt_transport ||
+                  this->get_parameters().compositional_field_methods[porosity_idx] == Parameters<dim>::AdvectionFieldMethod::fem_darcy_field,
+                  ExcMessage("The 'fluid velocity statistics' postprocessor requires advecting the 'porosity' compositional "
+                             "field by either the darcy velocity or the melt velocity."));
+
+      const bool output_melt_velocity = this->get_parameters().include_melt_transport;
       const Quadrature<dim> &quadrature_formula = this->introspection().quadratures.velocities;
       const unsigned int n_q_points = quadrature_formula.size();
+
+      const UpdateFlags update_flags
+        = UpdateFlags(
+            !output_melt_velocity
+            ?
+            update_values |
+            update_gradients |
+            update_quadrature_points |
+            update_JxW_values
+            :
+            update_values |
+            update_JxW_values);
 
       FEValues<dim> fe_values (this->get_mapping(),
                                this->get_fe(),
                                quadrature_formula,
-                               update_values   |
-                               update_gradients |
-                               update_quadrature_points |
-                               update_JxW_values);
+                               update_flags);
+
       std::vector<Tensor<1,dim>> velocity_values(n_q_points);
       std::vector<Tensor<1,dim>> fluid_velocity_values(n_q_points);
 
       MaterialModel::MaterialModelInputs<dim> in(fe_values.n_quadrature_points, this->n_compositional_fields());
       MaterialModel::MaterialModelOutputs<dim> out(fe_values.n_quadrature_points, this->n_compositional_fields());
       MeltHandler<dim>::create_material_model_outputs(out);
-      MaterialModel::MeltOutputs<dim> *fluid_out = out.template get_additional_output<MaterialModel::MeltOutputs<dim>>();
 
       double local_fluid_velocity_square_integral = 0;
       double local_max_fluid_velocity = 0;
-
-      unsigned int porosity_idx = numbers::invalid_unsigned_int;
-      if (this->introspection().composition_type_exists(CompositionalFieldDescription::porosity))
-        {
-          porosity_idx = this->introspection().find_composition_type(CompositionalFieldDescription::porosity);
-        }
 
       for (const auto &cell : this->get_dof_handler().active_cell_iterators())
         if (cell->is_locally_owned())
           {
             fe_values.reinit (cell);
-            fe_values[this->introspection().extractors.velocities].get_function_values (this->get_solution(),
-                                                                                        velocity_values);
-            in.reinit(fe_values, cell, this->introspection(), this->get_solution());
-            this->get_material_model().evaluate(in, out);
-            fluid_out = out.template get_additional_output<MaterialModel::MeltOutputs<dim>>();
-            for (unsigned int q = 0; q < n_q_points; ++q)
+
+            if (output_melt_velocity)
               {
-                const Tensor<1,dim> gravity = this->get_gravity_model().gravity_vector(fe_values.quadrature_point(q));
-                const double porosity = std::max(in.composition[q][porosity_idx], 1e-10);
-                const double solid_density = out.densities[q];
-                const double fluid_density = fluid_out->fluid_densities[q];
-                const double fluid_viscosity = fluid_out->fluid_viscosities[q];
-                const double permeability = fluid_out->permeabilities[q];
-                fluid_velocity_values[q] = velocity_values[q] -
-                                           (permeability / fluid_viscosity / porosity) *
-                                           gravity * (solid_density - fluid_density);
-                local_fluid_velocity_square_integral += ((fluid_velocity_values[q] * fluid_velocity_values[q]) *
-                                                         fe_values.JxW(q));
-                local_max_fluid_velocity = std::max (std::sqrt(fluid_velocity_values[q]*fluid_velocity_values[q]),
-                                                     local_max_fluid_velocity);
+                const FEValuesExtractors::Vector ex_u_f = this->introspection().variable("fluid velocity").extractor_vector();
+                fe_values[ex_u_f].get_function_values (this->get_solution(), fluid_velocity_values);
+                for (unsigned int q = 0; q < n_q_points; ++q)
+                  {
+                    local_fluid_velocity_square_integral += ((fluid_velocity_values[q] * fluid_velocity_values[q]) *
+                                                             fe_values.JxW(q));
+                    local_max_fluid_velocity = std::max (std::sqrt(fluid_velocity_values[q]*fluid_velocity_values[q]),
+                                                         local_max_fluid_velocity);
+                  }
+              }
+
+            else
+              {
+                fe_values[this->introspection().extractors.velocities].get_function_values (this->get_solution(),
+                                                                                            velocity_values);
+                in.reinit(fe_values, cell, this->introspection(), this->get_solution());
+                this->get_material_model().evaluate(in, out);
+                MaterialModel::MeltOutputs<dim> *fluid_out = out.template get_additional_output<MaterialModel::MeltOutputs<dim>>();
+                for (unsigned int q = 0; q < n_q_points; ++q)
+                  {
+                    const Tensor<1,dim> gravity = this->get_gravity_model().gravity_vector(fe_values.quadrature_point(q));
+                    const double porosity = std::max(in.composition[q][porosity_idx], 1e-10);
+                    const double solid_density = out.densities[q];
+                    const double fluid_density = fluid_out->fluid_densities[q];
+                    const double fluid_viscosity = fluid_out->fluid_viscosities[q];
+                    const double permeability = fluid_out->permeabilities[q];
+                    fluid_velocity_values[q] = velocity_values[q] -
+                                               (permeability / fluid_viscosity / porosity) *
+                                               gravity * (solid_density - fluid_density);
+                    local_fluid_velocity_square_integral += ((fluid_velocity_values[q] * fluid_velocity_values[q]) *
+                                                             fe_values.JxW(q));
+                    local_max_fluid_velocity = std::max (std::sqrt(fluid_velocity_values[q]*fluid_velocity_values[q]),
+                                                         local_max_fluid_velocity);
+                  }
               }
           }
 
@@ -99,8 +131,9 @@ namespace aspect
 
       const std::string units = (this->convert_output_to_years() == true) ? "m/year" : "m/s";
       const double unit_scale_factor = (this->convert_output_to_years() == true) ? year_in_seconds : 1.0;
-      const std::vector<std::string> column_names = {"RMS fluid velocity (" + units + ")",
-                                                     "Max. fluid velocity (" + units + ")"
+      const std::string advection_method = output_melt_velocity ? "melt" : "Darcy";
+      const std::vector<std::string> column_names = {"RMS " + advection_method + " velocity (" + units + ")",
+                                                     "Max. " + advection_method + " velocity (" + units + ")"
                                                     };
 
       statistics.add_value (column_names[0],
@@ -123,7 +156,7 @@ namespace aspect
              << global_max_fluid_velocity *unit_scale_factor
              << ' ' << units;
 
-      return std::pair<std::string, std::string> ("RMS fluid, max fluid velocity:",
+      return std::pair<std::string, std::string> ("RMS " + advection_method + ", max " + advection_method + " velocity:",
                                                   output.str());
     }
   }
